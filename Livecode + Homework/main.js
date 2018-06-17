293153a4ea6ac08e5db41bce86024818409bfd0f
Luay Adam <luay985@gmail.com>
2018-06-18 00:52:51 +0200

    changed 3rd condition statement

--- Livecode + Homework/main.js
+++ Livecode + Homework/main.js
@@ -1,12 +1,10 @@
 for (let i = 1; i <= 100; i++) {
   console.log(i);
-  if (i % 3 == 0) {
-    console.log("Hack");
-  }
-  if (i % 5 == 0) {
-    console.log("Future");
-  }
-  if (i % 8 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
     console.log("HackYourFuture");
+  } else if (i % 3 == 0) {
+    console.log("Hack");
+  } else if (i % 5 == 0) {
+    console.log("Future");
   }
 }
