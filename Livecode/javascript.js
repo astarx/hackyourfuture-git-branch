141c95aace2f3acb861d868b9811d21a3ebb9f2c
Luay Adam <luay985@gmail.com>
2018-06-17 22:02:33 +0200

    adding first part of the homework

---
+++ Livecode/javascript.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i % 3 == 0) {
+    console.log("Hack");
+  }
+  if (i % 5 == 0) {
+    console.log("Future");
+  }
+  if (i % 8 == 0) {
+    console.log("HackYourFuture");
+  }
+}
