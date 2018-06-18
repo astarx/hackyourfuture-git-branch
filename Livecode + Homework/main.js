42f830e3c2092064d2e48dccc9dab94240bf86c2
Luay Adam <luay985@gmail.com>
2018-06-18 16:45:05 +0200

    added extra solutions

--- Livecode + Homework/main.js
+++ Livecode + Homework/main.js
@@ -1,6 +1,6 @@
 for (let i = 1; i <= 100; i++) {
   console.log(i);
-  if (i % 3 == 0 && i % 5 == 0) {
+  if (i % 15 == 0) {
     console.log("HackYourFuture");
   } else if (i % 3 == 0) {
     console.log("Hack");
@@ -8,3 +8,17 @@ for (let i = 1; i <= 100; i++) {
     console.log("Future");
   }
 }
+
+// Google search result 
+// for (var i = 0; ++i <= 100;) {
+//   var a = i % 3 == 0, b = i % 5 == 0;
+//   console.log(a ? b ? "HackYourFuture" : "Hack" : b ? "Future" : i);
+// }
+// or with extra variable
+// for (var i = 0; ++i <= 100;) {
+//   var a = i % 3 == 0, b = i % 5 == 0, c = i % 15 == 0;
+//   console.log(c ? "HackYourFuture" : a ? "Hack" : b ? "Future" : i);
+// }
+
+// Google search result (Unreadable)
+// for(var i=1;100>=i;++i)console.log(i%15!==0?i%5!==0?i%3===0?"Hack":i:"Future":"HackYourFute");
