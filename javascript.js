16b1bcd82c5e98bbc8d017650ca9e6bf4fb8bac2
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-06-20 11:29:28 +0200

    multiples of both 3 and five

--- javascript.js
+++ javascript.js
@@ -11,10 +11,17 @@
 //         }
         
 // }
-// multiples of five up tp 200,
+// // multiples of five up tp 200,
+// for (var x=1; x <= 200; x++){
+//     if( x % 5 == 0 ) {
+//         console.log(x + "","log", "future");
+//         }
+        
+// }
+// multiples of both three and five up tp 200,
 for (var x=1; x <= 200; x++){
-    if( x % 5 == 0 ) {
-        console.log(x + "","log", "future");
+    if( x % 3 == 0 &&  x%5==0) {
+        console.log(x + "","log", "hackyourfuture");
         }
         
 }
