af8db1e07bc79b931c7c9161023adee3d5a682c4
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-06-20 23:19:37 +0200

    the javacript1 is for the extar  homework2

--- javascript.js
+++ javascript.js
@@ -1,23 +1,23 @@
-// // number from one to 100
-//  for(i=1; i<= 100; i++){
-//     console.log(i);
+// number from one to 100
+ for(i=1; i<= 100; i++){
+    console.log(i);
 
-// }
+}
 
-// // multiples of three up tp 200,
-// for (var x=1; x <= 200; x++){
-//     if( x % 3 == 0 ) {
-//         console.log(x + "","log", "hack");
-//         }
+ // multiples of three up tp 200,
+ for (var x=1; x <= 200; x++){
+     if( x % 3 == 0 ) 
+      console.log(x + "","log", "hack");
+        }
         
-// }
-// // multiples of five up tp 200,
-// for (var x=1; x <= 200; x++){
-//     if( x % 5 == 0 ) {
-//         console.log(x + "","log", "future");
-//         }
+}
+// multiples of five up tp 200,
+for (var x=1; x <= 200; x++){
+    if( x % 5 == 0 ) {
+        console.log(x + "","log", "future");
+        }
         
-// }
+}
 // multiples of both three and five up tp 200,
 for (var x=1; x <= 200; x++){
     if( x % 3 == 0 &&  x%5==0) {
---
+++ javascript1.js
@@ -0,0 +1,38 @@
+
+// 1. Write a console.log statement saying "Hello World!" for each language that you know1.
+console.log("bonjour le monde || french ");
+console.log("Hello World || English ");
+console.log("Barka de Soubha || Haussa ");
+console.log("Aren Kani Bani|| Zerma ");
+
+// 2. Consider the following code and correction
+console.log( " I'm awesome");
+
+
+
+// console.log('test');
+// var array= [
+
+
+// 5,
+// 6,
+// "hackyo",
+// "future"
+// ];
+// array.forEach((Element) =>{
+//     console.log(Element);
+// })
+
+// console.log(array.length)
+
+// for(let i=0; i<array.length; i++){
+//     console.log(array[i])
+// }
+
+// console.log("am aweson");
+// var x;
+// console.log("the value of my variable wil be undefined");
+// var x= 2;
+// console.log(x);
+
+
