be5cba9850ed6e55fb4822f050283f1da27486f9
Abdou Sanda Dourhamane <abdous2004@yahoo.fr>
2018-06-20 11:21:03 +0200

    multiples of 3

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-for(i=1; i<= 100; i++){
-    console.log(i);
+// // number from one to 100
+//  for(i=1; i<= 100; i++){
+//     console.log(i);
 
+// }
+
+// multiples of three up tp 200,
+for (var x=1; x <= 200; x++){
+    if( x % 3 == 0 ) {
+        console.log(x + "","log", "hack");
+        }
+        
 }
