ef4a44cd3964b305c0eafcdc41bdc5865307ae69
radwan mohammed <mohammedfradwan@gmail.com>
2018-06-21 13:31:57 +0200

    Add ForLoop HomeWork

---
+++ javaScriptForLoop.js
@@ -0,0 +1,6 @@
+
+for(let m=1;m<=100;m++){
+    if(m%5==0&&m%3==0){console.log(m+ 'hackyourfuture');}
+    else if(m%3==0){console.log(m + 'hack');}
+    else if(m%5==0){console.log(m + 'future');}
+}
