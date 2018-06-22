e30c4b3d946b7a591dacdaffc4ffafa6c02f3a37
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-06-22 21:43:05 +0200

    1st attemp (failed)

---
+++ homework.js
@@ -0,0 +1,14 @@
+var i = 50;
+while (i <= 100){
+    console.log(i);
+    i++;
+    while (i % 3 == 0) {
+        console.log(HACK);
+        while(i % 5 == 0) {
+            console.log(FUTURE);
+            while(i % 5 == 0, i % 3 == 0) {
+                console.log(HACKYOURFUTURE);
+            }
+        }
+    }
+}
