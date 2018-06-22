d5cb247aba974ee9b7f1a2f3728b96c081987416
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-06-22 22:20:31 +0200

    second attemp (successful but long

--- homework.js
+++ homework.js
@@ -1,14 +1,11 @@
-var i = 50;
-while (i <= 100){
-    console.log(i);
-    i++;
-    while (i % 3 == 0) {
-        console.log(HACK);
-        while(i % 5 == 0) {
-            console.log(FUTURE);
-            while(i % 5 == 0, i % 3 == 0) {
-                console.log(HACKYOURFUTURE);
-            }
-        }
+for (x=1; x <= 100; x++){
+    if( x % 3 == 0 ){
+        console.log("Hack")
+    }
+    if( x % 5 == 0 ){
+        console.log("Future")
+    }
+    if( ( x % 3 == 0 ) && ( x % 5 == 0 ) ){
+        console.log("Hackyourfuture")
     }
 }
