5415cff4d40e38a2cb6bbcaefdf10d08eb5fb0cb
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-06-24 01:30:01 +0200

    Create loop.js

---
+++ Week 5/loop.js
@@ -0,0 +1,4 @@
+for(var i = 1; i<100; i++){if(i%3 === 0 && i % 5 === 0){console.log("hackyourfuture");}
+    else if (i % 3 === 0){console.log("hack");}
+    else(i % 5 == 0);{console.log("future")}
+    console.log(i);}
