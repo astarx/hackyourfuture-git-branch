1274db89ab6bcd37db77a98eb481d4772507608d
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-06-26 15:53:02 +0200

    homework 5

--- Forloop.js
+++ Forloop.js
@@ -1,1 +1,76 @@
+//1
+console.log("hello, world"); //English
+console.log("merhaba bl alam"); //Arabic
+console.log("bonjour le monde"); //French
+console.log("hallo wereld"); //Dutch
 
+//2
+console.log('I\'m awesome');
+
+//3
+var x = 5;
+console.log(x);
+
+//4
+var y = "Hello";
+console.log("the value is Hallo");
+console.log(y);
+var y = "bye";
+console.log("the value of y will be bye");
+
+//5
+var z = 7.25;
+console.log(z);
+var a = Math.round(z);
+console.log(a);
+var s = Math.max(a, z);
+console.log(s);
+
+//6
+var animalLover = ["dogs", "lions", "dolphins", "wolves"];
+console.log(animalLover);
+animalLover.push("baby pig");
+console.log(animalLover);
+
+//7
+let myString = "this is a test";
+console.log(myString);
+myString = myString.length;
+console.log(myString);
+
+//8
+let p = "hi!";
+console.log("we say " + p);
+let Friends = 5;
+console.log("we are " + Friends);
+let Fights = false;
+console.log(Fights);
+let study = "javascript";
+console.log("i am studying " + study);
+
+if (typeof p === typeof Friends) {
+    console.log("typical");
+} else {
+    console.log("not the same");
+}
+
+//9
+x = 7 % 3;
+console.log(1);  // 7 / 3 = 2 , 2 * 3 = 6, 7- 6 = 1 the remaining is 1 
+g = 4 % 5; 
+console.log(4); // 4/5 = 0 so the remaining is 4-0=4
+k = 15 % 6;
+console.log(3); // 15/6= 2, 2*6= 12, the remaning is 15-12= 3
+
+//10
+var array = ["hello", 5, false];
+a = 6/0;
+c = 10/0;
+if (a === c) {
+    console.log("equal");
+} else {
+    console.log("hello no");
+}
+
+//the link of this homework (https://github.com/HackYourFuture/JavaScript1/blob/master/Week2/MAKEME.md)
+    
